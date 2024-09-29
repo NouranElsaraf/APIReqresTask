0c0dc7c667db10d65ab804b1a16032604e83d5a2
NouranElsaraf <111913684+NouranElsaraf@users.noreply.github.com>
2024-09-29 05:46:51 +0300

    Update RestClient.java

--- ApiTask/src/main/java/Utils/RestClient.java
+++ ApiTask/src/main/java/Utils/RestClient.java
@@ -6,17 +6,14 @@ import static io.restassured.RestAssured.given;
 
 public class RestClient {
 
-    // GET request with query parameters
     public static Response get(String endpoint, Map<String, Object> queryParams) {
         return given().header("Content-Type", "application/json").queryParams(queryParams).when().get(endpoint);
     }
 
-    // Generic method to send a POST request with a body
     public static Response post(String endpoint, String body) {
         return given().header("Content-Type", "application/json").body(body).when().post(endpoint);
     }
 
-    // Generic method to send a PUT request with a body
     public static Response put(String endpoint, String body) {
         return given().header("Content-Type", "application/json").body(body).when().put(endpoint);
     }
